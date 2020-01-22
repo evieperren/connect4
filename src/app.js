3385821ce6fc15dc84434928254235b8f1c3dd48
Evie Perren <evie.perren@simplyhealth.co.uk>
2020-01-22 22:49:04 +0000

    creating counters on click

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 let hoverElement = document.querySelectorAll('.hover')
+let hoverTarget = document.querySelectorAll('div[data-target^=".square--"]')
+let dataVal = document.querySelectorAll('div[data-value]')
 
 hasCounter = false
 hoverElement.forEach(hover => hover.addEventListener('mouseover', function(e){
@@ -10,6 +12,29 @@ hoverElement.forEach(hover => hover.addEventListener('mouseleave', function(e){
     e.stopPropagation()
     hover.className = 'game__hover-section--square hover'
 }))
-hoverElement.forEach(hover => hover.addEventListener('click', function(){
+hoverElement.forEach(hover => hover.addEventListener('click', function(e){
     e.target.className = 'game__hover-section--square--counter'
 }))
+
+let dataNum = document.querySelectorAll('div[data-num]')
+
+hoverTarget.forEach(targ => {
+    targ.addEventListener('click', function(e) {
+        let target = e.target.dataset.target
+        let targetedSquare = document.querySelectorAll(target)
+        targetedSquare.forEach(square => {
+            dataNum.forEach(num => {
+                if(row1[num.dataset.num] === 1){
+                    square.className = 'game__hover-section--square--counter' 
+                }
+            })
+            // if()
+            square.setAttribute('data-value', '1')
+            console.log(square)
+        })
+    })
+})
+
+
+let row2 = [0, 1, 0, 0, 0, 0, 0]
+let row1 = [0, 1, 0, 0, 0, 0, 0]
